847ff8ce16187fe56b90d7a81b34a75d9b28a273
Graham Fairweather <xotic750@gmail.com>
2019-07-22 00:52:37 +0200

    :bookmark: v2.0.9
    Auto build

--- dist/is-error-x.js
+++ dist/is-error-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-19T22:03:41.922Z",
+  "date": "2019-07-21T22:52:31.435Z",
   "describe": "",
   "description": " Detect whether a value is an error.",
   "file": "is-error-x.js",
   "hash": "190cf97d178fcc0f105f",
   "license": "MIT",
-  "version": "2.0.8"
+  "version": "2.0.9"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
